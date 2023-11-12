cccff683d6ef62cb3b443cf6bd99378b25d141a7
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-11-12 21:08:36 +0600

    add delete route

--- index.js
+++ index.js
@@ -76,14 +76,19 @@ async function run() {
         app.patch('/update-user-role/:id', async (req, res) => {
             const id = req.params.id;
             const data = req.body;
-            console.log(data)
             const filter = { _id: new ObjectId(id) };
             const updateInfo = {
-                $set: { role : data.role}
+                $set: { role: data.role }
             }
             const result = await loginUsers.updateOne(filter, updateInfo);
             res.send(result)
         });
+        app.delete('/remove-login-user/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const result = await loginUsers.deleteOne(filter);
+            res.send(result)
+        })
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
